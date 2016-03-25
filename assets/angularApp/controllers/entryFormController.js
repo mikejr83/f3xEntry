bca9b2bffdbd60c7f6ab8215c54938916db84e05
Michael Gardner <mikejr83@gmail.com>
2016-03-25 12:02:22 -0400

    working on start date and end date.

--- assets/angularApp/controllers/entryFormController.js
+++ assets/angularApp/controllers/entryFormController.js
@@ -1,7 +1,19 @@
 angular.module('app')
-  .controller('EntryFormController', ['$scope', 'CONTEST_TYPES', function($scope, contestTypes) {
+  .controller('EntryFormController', ['$scope', '$log', '$localStorage', 'CONTEST_TYPES', function($scope, $log, $localStorage, contestTypes) {
+    $scope.$storage = $localStorage.$default({
+      contests: [
+        {
+          name: null,
+          startDate: new Date(),
+          endDate: new Date(),
+          contestTypes: contestTypes[0]
+        }
+      ]
+    });
+
     $scope.showJumbo = true;
     $scope.contestTypes = contestTypes;
+    $scope.selectedContest = $scope.$storage.contests[0];
 
     $scope.lol = 'lol lol';
 

0835e3afa639a5b64da9239dca0082f7c821e94c
Michael Gardner <mikejr83@gmail.com>
2016-03-26 22:13:47 -0400

    starting the f3j/td calculation work

--- assets/angularApp/services/contestTypeFactory.js
+++ assets/angularApp/services/contestTypeFactory.js
@@ -20,10 +20,44 @@ angular.module('app')
               _.forEach(contest.rounds, function(round) {
                 $log.debug('Target for ' + round.number, round.targetTime);
 
-                targetTimes.push(round.targetTime);
+                targetTimes.push(moment.duration('00:' + round.targetTime));
               });
 
               $log.debug('Target times', targetTimes);
+
+
+              _.forEach(contest.pilots, function (pilot) {
+
+                var rounds = [];
+                _.forEach(pilot.rounds, function (round, index) {
+
+                  var pilotRoundTime = moment.duration('00:' + round.time);
+
+                  var group = 1;
+                  if (round.group !== undefined && round.group != null) {
+                    group = round.group;
+                  }
+
+                  var landing = 0;
+                  if (round.landing !== undefined && round.landing != null) {
+                    landing = parseInt(round.landing, 10);
+                  }
+
+                  var over = '';
+                  if (pilotRoundTime.asSeconds() > targetTimes[index].asSeconds()) {
+                    over = '1';
+                  }
+
+                  var penalty = '';
+                  if (round.penalty !== undefined && round.penalty != null) {
+                    penalty = round.penalty;
+                  }
+
+                  rounds.push(group + ',' + pilotRoundTime.minutes() + ',' + pilotRoundTime.seconds() + ',' + landing + ',' + over + ',' + penalty);
+                });
+
+                console.log('Rounds', rounds);
+              });
             }
         },
           {

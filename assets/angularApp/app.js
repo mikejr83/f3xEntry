969ab7bf4f9ea935841fd6a34cd35c0a64f07028
Michael Gardner <mikejr83@gmail.com>
2016-04-01 19:05:28 -0400

    Add f3k types.

--- assets/angularApp/app.js
+++ assets/angularApp/app.js
@@ -7,4 +7,46 @@ var app = angular.module('app', ['ngStorage', 'ngTable', 'mgcrea.ngStrap', 'ui.m
     '2.4GHz',
     '72MHz',
     'Other'
+  ])
+  .constant('F3K_ROUND_TYPES', [
+    {
+      name: 'A',
+      description: 'Last flight'
+    },
+    {
+      name: 'B',
+      description: 'Next to last and last flight'
+    },
+    {
+      name: 'C',
+      description: 'All up, last down, seconds'
+    },
+    {
+      name: 'D',
+      description: 'Increasing time by 15 seconds'
+    },
+    {
+      name: 'E',
+      description: 'Poker - variable target time'
+    },
+    {
+      name: 'F',
+      description: '3 out of 6'
+    },
+    {
+      name: 'G',
+      description: 'Five longest flights'
+    },
+    {
+      name: 'H',
+      description: 'One, two, three and four minute flights, any order'
+    },
+    {
+      name: 'I',
+      description: 'Three longest flights'
+    },
+    {
+      name: 'J',
+      description: 'Three last flights'
+    }
   ]);
